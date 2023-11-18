7564ef2a09ef932cd3ba51030c7e9d2d8f9054e8
RyuNumchon <ryuthailand@gmail.com>
2023-11-19 00:01:59 +0700

    Added headings and routes to other pages

--- flaskWeb.py
+++ flaskWeb.py
@@ -5,13 +5,10 @@ import json
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/recommend/")
 @app.route("/index")
 def index():
     return render_template("index.html")
-
-@app.route("/temp-plot")
-def temp_plot():
-    return render_template("temp-plot.html")
 
 @app.route("/recommendation", methods=["POST", "GET"])
 def recommendation():
@@ -21,7 +18,7 @@ def recommendation():
     else:
         return render_template("index.html")
 
-@app.route("/anime/<anime_name>")
+@app.route("/recommend/<anime_name>")
 def anime(anime_name):
     api_url = "http://54.164.159.198/anime_recommendation"
     data = {"anime_name": anime_name}
@@ -33,6 +30,35 @@ def anime(anime_name):
         return render_template("recommendation.html", data=recommendation_data)
     else:
         return render_template("error.html")
+
+@app.route("/overall")
+def overall():
+    return render_template("overall.html")
+
+@app.route("/2020")
+def page2020():
+    return render_template("page2020.html")
+
+@app.route("/2021")
+def page2021():
+    return render_template("page2021.html")
+
+@app.route("/2022")
+def page2022():
+    return render_template("page2022.html")
+
+@app.route("/2023")
+def page2023():
+    return render_template("page2023.html")
+
+@app.route("/ref")
+def ref():
+    return render_template("ref.html")
+
+@app.route("/temp-plot")
+def temp_plot():
+    return render_template("temp-plot.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
